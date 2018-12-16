9fcc5c850f591f8576c66f96b0a4ff4ec7e1996b
Kirill Vorobyev <kirill.vorobyev.313@gmail.com>
2018-12-16 13:13:52 -0500

    Day-14 Part 2 Solution

--- 2018/Day 14/index-partTwo.js
+++ 2018/Day 14/index-partTwo.js
@@ -1,20 +1,16 @@
-let hotChocolates = [];
+let hotChocolates = [3,7];
 let elves = [];
-
-let initialState = '37';
-hotChocolates = initialState;
-compute();
 
 compute = () => {
     //initialize elves as integers
     initElves();
     let recipeSequence = '768071';
-    while(!hotChocolates.substring(hotChocolates.length - 10).includes(recipeSequence)){
+    while(!hotChocolates.slice(-10).join('').includes(recipeSequence)){
         combineRecipes();
         pickNewRecipes();
     }
     //print solution
-    let prefix = hotChocolates.split(recipeSequence)[0];
+    let prefix = hotChocolates.join('').split(recipeSequence)[0];
     console.log('solution: '+prefix.length);
 }
 
@@ -33,8 +29,11 @@ combineRecipes = () => {
             elf2 : hotChocolates[elves[elf+1]]
         }
         let newHotChocolate = parseInt(scores.elf1) + parseInt(scores.elf2);
+        let newHotChocolateAsArray = (''+newHotChocolate).split('');
         //console.log('elf1: '+scores.elf1,'elf2: '+scores.elf2,'newCombination: '+newHotChocolate);
-        hotChocolates+=newHotChocolate;
+        for(let c of newHotChocolateAsArray){
+            hotChocolates[hotChocolates.length]=c;
+        }
     }
 }
 
@@ -49,3 +48,5 @@ pickNewRecipes = () => {
     elves = newElves;
     //console.log(elves);
 }
+
+compute();
